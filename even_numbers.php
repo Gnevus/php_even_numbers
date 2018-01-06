2b2ea53ad004457ca80a1a5fe17c10d5cfc029f9
Airat Nizamov <random21@ya.ru>
2018-01-06 16:23:24 +0400

    test commit

--- even_numbers.php
+++ even_numbers.php
@@ -1,6 +1,6 @@
-    <?
-    $massiv = array(1,2,3,4,5,6,7,8,9);
-    $sum = 0;
-    foreach ($massiv as $int_value) {
-        $int_value%2 == 0 ? $sum+=$int_value : $sum+=0;
-    }
+<?
+$massiv = array(1,2,3,4,5,6,7,8,9);
+$sum = 0;
+foreach ($massiv as $int_value) {
+    $int_value%2 == 0 ? $sum+=$int_value : $sum+=0;
+}
